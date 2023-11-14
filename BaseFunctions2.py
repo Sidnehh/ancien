11df40ea2b2aa175e589d3a528e7ee84ee04be51
Arthur S <arsmadja92@gmail.com>
2023-11-14 21:33:05 +0100

    punctuation removed

--- BaseFunctions2.py
+++ BaseFunctions2.py
@@ -5,14 +5,15 @@ import os
 
 presidentsNames = {"Chirac": "Jacques", "Giscard dEstaing": "Valérie", "Hollande": "François", "Macron": "Emmanuel",
                    "Mitterrand": "François", "Sarkozy": "Nicolas"}
-punctuation = ['!', '"','#','$','%','&',"'",')','*','+',',','-','.','/']
+punctuation_to_space = ['!', '"','#','$','%','&',"'",')','*','+',',','-','.','/']
+punctuation_to_delete = []
 
 
 ########################################################################################################################
-#Fonctions complémentaires (pas demandées dans la consigne)
+#FONCTIONS COMPLEMENTAIRES (pas demandées dans la consigne)
 ########################################################################################################################
 
-def CreateFolder(folder):  # Crée un dossier dans le répertoire actuel, ayant pour nom l'argument folder.
+def createFolder(folder):  # Crée un dossier dans le répertoire actuel, ayant pour nom l'argument folder.
     try:
         if not os.path.exists(folder):
             os.makedirs(folder)
@@ -27,7 +28,7 @@ def getTextFilesName(folder, extension="txt"):  # Retourne une liste des noms de
     return files_name
 
 ########################################################################################################################
-#Fonctions de base
+#FONCTIONS DE BASE
 ########################################################################################################################
 
 def extractNames(folder, extension="txt"):            # Retourne une de tous les noms des présidents
@@ -43,40 +44,63 @@ def extractNames(folder, extension="txt"):            # Retourne une de tous les
 
     return presidents
 
+def presidentsSpeeches(presidents, folder):
+    president_speeches = {}
+    for president in presidents:
+        for file in getTextFilesName(folder):
+            if president in file:
+                if(president in president_speeches.keys()):
+                    president_speeches[president].append(file)
+                else:
+                    president_speeches[president] = [file]
+
+    return president_speeches
+
 def associateName(lastname):            # Associe un prénom à un nom de président.
     return presidentsNames[lastname]
 
-def ShowNames(folder):                  # Affiche la liste des noms des présidents extraits
+def showNames(folder):                  # Affiche la liste des noms des présidents extraits
     print(extractNames(folder))         # des fichiers.
 
 def createCleanedDirectory():                # Crée le dossier "cleaned" et renvoie
     cleanFolder = "./cleaned/"               # l'emplacement de ce dossier.
-    CreateFolder(cleanFolder)
+    createFolder(cleanFolder)
     return cleanFolder
 
 def createCleanedFiles(files, basefolder):              # Crée le dossier "cleaned" et y place des copies
     cleanedfolder = createCleanedDirectory()            # des fichiers files, avec son contenu en minuscules.
     for file in files:
-        basepath = "./"+basefolder+"/"+file
+        basepath = basefolder+"/"+file
         with open(basepath, 'r') as base:
             lines = base.readlines()
-            for line_id, line in enumerate(lines):
-                lines[line_id] = line.lower()
-        cleanedpath = "./"+cleanedfolder+"/"+file
-        with open(cleanedpath, 'w+') as clean:
-            for line in lines:
-                clean.write(line)
+            for line in range(len(lines)):
+                lines[line] = list(lines[line])
+                for character in range(len(lines[line])):
+                    if 'A'<=lines[line][character]<='Z':
+                        lines[line][character] = chr(ord(lines[line][character])-(ord('A')-ord('a')))
+                lines[line] = ''.join(lines[line])
+
+
+            cleanedpath = "" + cleanedfolder + "/"+file
+            with open(cleanedpath, 'w+') as clean:
+                for line in lines:
+                    clean.write(line)
 
 def removePunctuation(cleanedfolder):                   #cette fonction ne marche pas, mais elle est censée
     cleaned_files = getTextFilesName(cleanedfolder)     #retirer la ponctuation de tous les fichiers d'un
     for file in cleaned_files:                          #dossier folder.
-        cleanedpath = "./" + cleanedfolder + "/" + file
+        cleanedpath = cleanedfolder + "/" + file
         with open(cleanedpath, "r") as clean:
             lines = clean.readlines()
-            for line in lines:
-                for symbol in punctuation:
-                    line = "".join(line.split(symbol))
-                    print(line)
+            
+            #for line in range(len(lines)):
+            #    for character in range(len(line)):
+            #        if lines[line][character] in punctuation:
+            for line in range(len(lines)):
+                for symbol in punctuation_to_space:
+                    lines[line] = " ".join(lines[line].split(symbol))
+            with open(cleanedpath, "w") as clean:
+                clean.writelines(lines)
 
 #pas fini, j'en ai marre !
 
--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+'''
+- Extraire les noms des présidents à partir des noms des fichiers texte fournis ;
+- Associer à chaque président un prénom ;
+- Afficher la liste des noms des présidents (attention aux doublons) ;
+- Convertir les textes des 8 fichiers en minuscules et stocker les contenus dans de nouveaux fichiers. Les
+nouveaux fichiers doivent être stockés dans un nouveau dossier appelé « cleaned ». Ce dossier doit se
+situer dans le répertoire principal où se trouve le programme main.py et au même niveau que le répertoire
+« speeches »
+- Pour chaque fichier stocké dans le répertoire « cleaned », parcourir son texte et supprimer tout caractère
+de ponctuation. Le résultat final doit donner un fichier avec des mots séparés par des espaces. Attention,
+certains caractères comme l’apostrophe (‘) ou le tiret (-) nécessitent un traitement spécial pour ne pas
+causer une concaténation de deux mots (exemple : « elle-même » devrait devenir « elle même » et non
+pas « ellemême »). Les modifications réalisées à cette étape devraient être stockées dans les mêmes
+fichiers du répertoire « cleaned ».
+'''
+
+from BaseFunctions2 import *
+
+basefolder = "speeches"
+cleanfolder = "cleaned"
+
+presidents_surnames = extractNames(basefolder, "txt")
+print(presidents_surnames)
+
+presidents_names_lastname = []
+for name in presidents_surnames:
+    presidents_names_lastname.append((associateName(name), name))
+print(presidents_names_lastname)
+
+presidents_speeches = presidentsSpeeches(presidents_surnames, basefolder)
+print(presidents_speeches, sep='\n')
+
+files = getTextFilesName(basefolder)
+
+createFolder(cleanfolder)
+createCleanedFiles(files, basefolder)
+removePunctuation(cleanfolder)
+
+
+
+
 
